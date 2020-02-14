5985af879d8b4c99601748ed5bfefefb2ac11bcf
Dean Pooler <Deanpooler@outlook.com>
2020-02-14 13:15:14 +0100

    Added comments

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,24 @@
-const createBtn = document.querySelector('#createGrid');
+const createBtn = document.querySelector("#createGrid");
 createBtn.addEventListener("click", createGrid);
 
 function createGrid() {
-    const container = document.querySelector(".container");
+    const container = document.querySelector("#container");
     const userInputGridSize = parseInt(prompt("How big should the grid's sides be?"));
     const gridSize = userInputGridSize * userInputGridSize;
     
     container.style.gridTemplateColumns =  "repeat(" + userInputGridSize + ", 1fr";
     container.style.gridTemplateRows =  "repeat(" + userInputGridSize + ", 1fr";
 
+    // Clear Etch-a-Sketch
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Create new Etch-a-Sketch
     for(let i = 0; i < gridSize; i++) {
         const div = document.createElement("div");
 
-        div.setAttribute("id", "divGrid" + i);
+        div.setAttribute("id", "divGrid");
 
         container.appendChild(div);
         div.addEventListener("mouseover", mouseOver);
